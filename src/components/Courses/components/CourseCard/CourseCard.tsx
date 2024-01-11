625eba49283e2823ef655c3eff23f62c7bc824fb
Nemanja <nemanjakostovski@gmail.com>
2024-01-11 15:58:56 +0100

    small w adgustment

--- src/components/Courses/components/CourseCard/CourseCard.tsx
+++ src/components/Courses/components/CourseCard/CourseCard.tsx
@@ -27,8 +27,8 @@ export default function CourseCard({
     <section className='flex flex-col p-2 border-2 border-l-8 border-l-black rounded w-5/6 mb-4 m-2 bg-white'>
       <h2 className='font-bold p-1'>{title}</h2>
       <article className='flex p-1 ml-1'>
-        <p className='w-2/3'>{text}</p>
-        <div className='flex flex-col w-1/3'>
+        <p className='w-[60%]'>{text}</p>
+        <div className='flex flex-col w-[40%}'>
           <p className='truncate'>
             <span className='font-bold'>Authors: </span>
             {author}
